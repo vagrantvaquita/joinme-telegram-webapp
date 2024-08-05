1dab9c899d09c6f7b1f1531ce2723a3f40db3f7b
Agustin RODRIGUEZ <vagrantvaquita@proton.me>
2024-08-04 21:57:42 -0300

    refactor(telegram): add JSON stringify to message passed to sendData

--- pages/event/create.vue
+++ pages/event/create.vue
@@ -8,14 +8,16 @@ import { ref, onMounted } from "vue"
 const message = ref("")
 
 onMounted(() => {
+    console.log(window.Telegram.WebApp);
     window.Telegram.WebApp.MainButton.setParams({
         text: "Create Event",
         is_active: true,
         is_visible: true,
-    })
+    });
     window.Telegram.WebApp.onEvent("mainButtonClicked", () => {
-        window.Telegram.WebApp.sendData({"message": message})
-    })
+        window.Telegram.WebApp.sendData(JSON.stringify({"message": message}))
+        window.Telegram.WebApp.close()
+    });
 })
 
 </script>
