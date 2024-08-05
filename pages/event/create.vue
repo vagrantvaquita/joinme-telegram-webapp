2d1483dbcbfcaf34288d7983354b3204f99bd60f
Agustin RODRIGUEZ <vagrantvaquita@proton.me>
2024-08-05 01:04:17 -0300

    refactor: add missing textbox and init style sheet

--- pages/event/create.vue
+++ pages/event/create.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
-        <p>Message is: {{ message }}</p>
-        <input v-model="message"  placeholder="Title">
-        <input placeholder="Description">
-        <input placeholder="URL">
-        <input type="date" min="2024-08-01">
-        <input type="time">
-    </div>
+    <form>
+        <input type="text" v-model="title" placeholder="Title">
+        <input type="text" v-model="description" placeholder="Description">
+        <input type="text" v-model="location" placeholder="Location">
+        <input type="datetime-local" v-model="datetime">
+        <input type="url" v-model="url" placeholder="URL">
+    </form>
 </template>
 
 <script setup>
 
 import { ref, onMounted } from "vue";
 
-const message = defineModel({default: ""});
+const title = ref("");
+const description = ref("");
+const location = ref("");
+const datetime = ref("");
+const url = ref("");
 
 onMounted(() => {
+    window.Telegram.WebApp.expand();
 
     window.Telegram.WebApp.MainButton.setParams({
         text: "Create Event",
@@ -28,7 +32,13 @@ onMounted(() => {
             "Are you sure you want to create this event?",
             (ok) => {
                 if (ok) {
-                    window.Telegram.WebApp.sendData(JSON.stringify({message: message.value}));
+                    window.Telegram.WebApp.sendData(JSON.stringify({
+                        title: title.value,
+                        description: description.value,
+                        location: location.value,
+                        datetime: datetime.value,
+                        url: url.value
+                    }));
                     window.Telegram.WebApp.close();
                 }
             }
@@ -37,3 +47,16 @@ onMounted(() => {
 })
 
 </script>
+
+<style>
+form {
+    padding: 0 25px 0 25px;
+}
+input {
+    display: block;
+    box-sizing: border-box;
+    margin: 20px 0 20px 0;
+    padding: 5px;
+    width: 100%;
+}
+</style>
